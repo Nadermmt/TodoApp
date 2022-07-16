b53e1bbe061881483c0871bd162c4df92b8dbea0
NaderMagdy <nadermmt@gmail.com>
2022-07-16 22:21:32 +0200

    Apply provider to App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import {styles} from "./App";
 import { RootNavigation } from "./src/navigation";
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
+import {styles} from "./AppStyles";
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <RootNavigation/>
-    </SafeAreaView>
+      <Provider store={store}>
+          <SafeAreaView style={styles.container}>
+              <RootNavigation/>
+          </SafeAreaView>
+      </Provider>
   );
 };
 
