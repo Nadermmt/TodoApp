6141c0d5ab5ffd784171bb708995dcc1166843b1
NaderMagdy <nadermmt@gmail.com>
2022-07-16 14:35:55 +0200

    Project init

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import {styles} from "./App";
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  );
+};
+
+export default App;
