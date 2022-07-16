a39c8c312c0c4e3d59c09f58dfc468872fc6f3dd
NaderMagdy <nadermmt@gmail.com>
2022-07-16 22:18:02 +0200

    Add bin, check, edit, emptyStar, exercise, filledStar, sleep, study and work images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,12 @@
 export const Images = {
-    todoList: require('./todo.png')
+    bin: require('./bin.png'),
+    check: require('./check.png'),
+    edit: require('./edit.png'),
+    emptyStar: require('./emptyStar.png'),
+    exercise: require('./exercise.png'),
+    filledStar: require('./filledStar.png'),
+    sleep: require('./sleep.png'),
+    study: require('./study.png'),
+    todoList: require('./todo.png'),
+    work: require('./work.png'),
 }
