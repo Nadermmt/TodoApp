825581bfd7431ba09f5b7b1dd6daa179b5e39bd2
NaderMagdy <nadermmt@gmail.com>
2022-07-16 22:18:53 +0200

    Add types and constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+import {Images} from "../assets";
+
+export const ImageChoices = [
+    {
+        source: Images.exercise
+    },
+    {
+        source: Images.sleep
+    },
+    {
+        source: Images.study
+    },
+    {
+        source: Images.work
+    }
+]
---
+++ src/utils/types.ts
@@ -0,0 +1,9 @@
+export type TodoItem = {
+    id: number;
+    desc: string;
+    isCompleted: boolean;
+    isImportant: boolean;
+    pic: string;
+    createdDate: string;
+    completedDate: string;
+}
