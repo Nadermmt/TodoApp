7455eb44451f1bd4eb8b128f5127ad79f4b77790
NaderMagdy <nadermmt@gmail.com>
2022-07-16 17:03:13 +0200

    Check authentication

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Image, View} from 'react-native';
 import {styles} from './styles';
 import {Images} from "../../assets";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Splash = () => {
+const Splash = ({ navigation }) => {
+    useEffect(() => {
+        setTimeout(() => {
+            isUserLoggedIn();
+        }, 1000)
+    }, []);
+
+    const isUserLoggedIn = async () => {
+        const isLoggedIn = await AsyncStorage.getItem('isUserLoggedIn')
+        if (isLoggedIn && JSON.parse(isLoggedIn)) {
+            navigation.navigate('Home');
+        } else {
+            navigation.navigate('Login');
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Image style={styles.todoListImage} source={Images.todoList}/>
